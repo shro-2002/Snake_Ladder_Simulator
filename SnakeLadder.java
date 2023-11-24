14e1d1dbe90d4ec3b1a22f2ce149b0a91f12e5ff
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-25 00:44:11 +0530

    Added UC-1

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,7 +4,20 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
+		Player();
+	}
 
+	/*
+	 * @params: void
+	 * 
+	 * @return: void
+	 * 
+	 * @description: UC-1: Snake and Ladder game played with single player at start
+	 * position 0
+	 */
+	public static void Player() {
+		int position = 0;
+		System.out.println("Player is at position: " + position);
 	}
 
 }
